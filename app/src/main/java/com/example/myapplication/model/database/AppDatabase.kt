488797ae0174d047b1eb5d8e7894c2524065db0c
vandanaBansal <vandanabansal377@gmail.com>
2024-08-05 20:29:32 +0530

    remove unwanted import

--- app/src/main/java/com/example/myapplication/model/database/AppDatabase.kt
+++ app/src/main/java/com/example/myapplication/model/database/AppDatabase.kt
@@ -1,13 +1,10 @@
 package com.example.myapplication.model.database
 
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.myapplication.model.dao.TodoDao
 import com.example.myapplication.model.data.TodoItem
-import com.example.myapplication.utils.Constants
 
 
 @Database(entities = [TodoItem::class], version = 1, exportSchema = false)
--- app/src/main/java/com/example/myapplication/view/ToDoApplication.kt
+++ app/src/main/java/com/example/myapplication/view/ToDoApplication.kt
@@ -1,10 +1,7 @@
 package com.example.myapplication.view
 
 import android.app.Application
-import com.example.myapplication.model.database.AppDatabase
-import com.example.myapplication.model.repository.TodoRepository
 import dagger.hilt.android.HiltAndroidApp
-
 
 @HiltAndroidApp
 class ToDoApplication:Application()
